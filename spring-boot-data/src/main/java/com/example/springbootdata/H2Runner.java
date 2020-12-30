1ccad9dc3d21a9c4ce054ac8ba3bc354b40caa5a
hwangyoungjin <innovation950302@gmail.com>
2020-12-30 23:56:55 +0900

    in memory

---
+++ spring-boot-data/src/main/java/com/example/springbootdata/H2Runner.java
@@ -0,0 +1,32 @@
+package com.example.springbootdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER(" +
+                    "ID INTEGER NOT NULL, " +
+                    "name VARCHAR(255), " +
+                    "PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+        }
+    }
+}
